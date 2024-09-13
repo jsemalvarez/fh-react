b390fb34d576362beae99cf67fdb199e3faa44b4
jsemalvarez <jsemalvarez@gmail.com>
2024-09-13 17:08:13 -0300

    first test

---
+++ 01_react_cero_experto/01_counter-app/test/base-pruebas/02-template-string.test.js
@@ -0,0 +1,12 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+
+describe('02-template-string', () => {
+
+    test('should getSaludo return "Hola josema"', () => { 
+
+        const name= 'josema';
+        const message = getSaludo( name )
+
+        expect( message ).toBe(`Hola ${name}`)
+     })
+})
