a4c5f1a08ceed00950b910379ab66b7628ea15dd
jsemalvarez <jsemalvarez@gmail.com>
2024-09-17 18:08:54 -0300

    test: render method

--- 01_react_cero_experto/01_counter-app/test/FirstApp.test.jsx
+++ 01_react_cero_experto/01_counter-app/test/FirstApp.test.jsx
@@ -3,25 +3,49 @@ const { FirstApp } = require("../src/FirstApp")
 
 describe('FirstApp.jsx',() => {
 
-    test('should be to match with the snapshot',() => {
-        const title = "Soy josema"
-        const { container } = render( <FirstApp title={title} /> )
-        expect( container ).toMatchSnapshot()
-    })
+    // no se recomienda hacer test de snapshot cuando estamos desarrollando la app
+    // test('should be to match with the snapshot',() => {
+    //     const title = "Soy josema"
+    //     const { container } = render( <FirstApp title={title} /> )
+    //     expect( container ).toMatchSnapshot()
+    // })
 
-    test('should show title in h1 tag',() => {
+    test('should display title in h1 tag',() => {
         const title = "Soy josema"
-        const { container, getByText } = render( <FirstApp title={title} /> )
+        const { getByText } = render( <FirstApp title={title} /> )
         expect( getByText(title)).toBeTruthy()
     })
 
-    test('should show title in h1 tag',() => {
+    // este tipo de pruebas, no es recomendable porque son poco flexibles, 
+    // ya que el h1 puede cambiar a un h2 o a un <p>
+    test('should display title in h1 tag',() => {
         const title = "Soy josema"
-        const { container, getByText } = render( <FirstApp title={title} /> )
+        const { container } = render( <FirstApp title={title} /> )
 
         const h1 = container.querySelector('h1')
         expect(h1.innerHTML).toBe( title )
+        // en el caso de que tenga espacios uno de los titulos,
+        // el test pasa
         expect(h1.innerHTML).toContain( title )
+    })
+
+    test('should display title in h1 tag',() => {
+        const title = "Soy josema"
+        const { getByTestId } = render( <FirstApp title={title} /> )
+
+        expect( getByTestId('test-title') ).toBeTruthy();
+        expect( getByTestId('test-title').innerHTML ).toBe( title );
+        expect( getByTestId('test-title').innerHTML ).toContain( title );
+    })
+
+    test('should display the subtitle thats comes int the props', ()=> {
+        const title = "Soy josema"
+        const subtitle = "titulo secundario"
+
+        const { getByText, getAllByText } = render( <FirstApp title={title} subtitle={subtitle} />)
+
+        expect( getByText(subtitle) ).toBeTruthy();
+        expect( getAllByText(subtitle).length ).toBe(1);
     })
 
 })
