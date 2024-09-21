1fe1d280875772e329b06511dd2da977986fc8cc
jsemalvarez <jsemalvarez@gmail.com>
2024-09-21 13:28:53 -0300

    fix: env GIFT_API_KEY

--- 01_react_cero_experto/02_gif-expert/src/helpers/getGifs.js
+++ 01_react_cero_experto/02_gif-expert/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const GIFT_API_KEY = import.meta.env.GIFT_API_KEY
+    const GIFT_API_KEY = import.meta.env.VITE_GIFT_API_KEY
 
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${ GIFT_API_KEY }&q=${ category }&limit=10`;
     const resp = await fetch( url );
