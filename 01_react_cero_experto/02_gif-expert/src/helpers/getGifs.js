9529fa53d8d1ba24adddc0efdb18591f98963d33
jsemalvarez <jsemalvarez@gmail.com>
2024-09-21 13:09:21 -0300

    feat: implement env variables

--- 01_react_cero_experto/02_gif-expert/src/helpers/getGifs.js
+++ 01_react_cero_experto/02_gif-expert/src/helpers/getGifs.js
@@ -1,6 +1,8 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${ category }&limit=10`;
+    const GIFT_API_KEY = import.meta.env.GIFT_API_KEY
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ GIFT_API_KEY }&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
