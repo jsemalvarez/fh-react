3ce49e25387884b66242a07d704111898bd45d54
jsemalvarez <jsemalvarez@gmail.com>
2024-09-24 10:20:17 -0300

    test: useCounter should increment

--- 01_react_cero_experto/03_hooks/tests/hooks/useCounter.test.js
+++ 01_react_cero_experto/03_hooks/tests/hooks/useCounter.test.js
@@ -1,4 +1,4 @@
-import { renderHook, waitFor } from "@testing-library/react"
+import { act, renderHook, waitFor } from "@testing-library/react"
 import { useCounter } from "../../src/hooks/useCounter"
 
 
@@ -23,5 +23,34 @@ describe('useCounter', () => {
         const { counter } = result.current; 
 
         expect( counter ).toBe( initialValue );
+    })
+
+
+    test('should increment', ()=> {
+
+        const initialValue = 100; 
+        const firstIncrement = 1;
+        const secondIncrement = 2;
+        const tatalResult = initialValue + firstIncrement + secondIncrement
+
+        const { result } = renderHook( () => useCounter( initialValue ) );
+        const { counter, increment } = result.current
+
+        // NOTA: para poder ejecutar en simultaneo el increment, el increment
+        // debe tomar el valor actual del counter, no el valor directamente del counter
+        // MODIFICAMOS el useCounter 
+        // const increment = ( value = 1 ) => {
+        //     // setCounter( counter + value );
+        //     setCounter( (current) => current + value ); // 
+        // } 
+        act( () => {
+            increment( firstIncrement )
+            increment( secondIncrement )
+        })
+        
+        // con valores promitivos, no se aplica referencia, 
+        // por eso tenemos que usar el result
+        // expect( counter ).toBe( tatalResult )
+        expect( result.current.counter ).toBe( tatalResult )
     })
 })
