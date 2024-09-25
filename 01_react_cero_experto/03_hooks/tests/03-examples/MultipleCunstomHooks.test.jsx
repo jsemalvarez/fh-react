86fb182a831a38183be12d7467517c042e29c43e
jsemalvarez <jsemalvarez@gmail.com>
2024-09-25 09:01:05 -0300

    test: MultipleCustomHooks should display a quote

--- 01_react_cero_experto/03_hooks/tests/03-examples/MultipleCunstomHooks.test.jsx
+++ 01_react_cero_experto/03_hooks/tests/03-examples/MultipleCunstomHooks.test.jsx
@@ -1,10 +1,18 @@
 import { render, screen } from "@testing-library/react"
 import { MultipleCustomHooks } from "../../src/03-examples"
+import { useFetch } from "../../src/hooks/useFetch"
 
+jest.mock('../../src/hooks/useFetch')
 
 describe('MultipleCustomHooks', () => {
 
     test('should render the initial state',() => {
+
+        useFetch.mockReturnValue({
+            data:null,
+            isLoading: true,
+            hasError: null
+        })
 
         render( <MultipleCustomHooks /> )
 
@@ -13,5 +21,23 @@ describe('MultipleCustomHooks', () => {
 
         const nextButton = screen.getByRole('button',{ name: 'Next quote'})
         expect( nextButton.disabled ).toBeTruthy()
+    })
+
+    test('should display a Quote', ()=> {
+
+        const mockQuote = {author:'josema', quote:'hola mundo'};
+        useFetch.mockReturnValue({
+            data:[mockQuote],
+            isLoading: false,
+            hasError: null
+        })
+
+        render( <MultipleCustomHooks /> );
+
+        expect( screen.getByText(mockQuote.quote) ).toBeTruthy()
+        expect( screen.getByText(mockQuote.author) ).toBeTruthy()
+        
+        const nextButton = screen.getByRole('button', {name: 'Next quote'})
+        expect( nextButton.disabled ).toBeFalsy()
     })
 })
